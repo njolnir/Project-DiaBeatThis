1970affd3dcd7d1c8fc7c5d0cb70be7090807d88
rron.ds <127083652+njolnir@users.noreply.github.com>
2023-04-12 22:50:23 +0800

    Add files via upload
    123

--- Dia-Beat-This.py
+++ Dia-Beat-This.py
@@ -125,7 +125,7 @@ st.markdown("""
 if st.button('Run MODEL'):
     # Make prediction using the trained model
     prediction_num = model.predict(df)[0]
-    pred_map = {1: 'Diabetic Risk', 0: 'Non-Diabetic'}
+    pred_map = {1: 'High Risk in Diabetes', 0: 'Low Risk in Diabetes'}
     prediction = pred_map[prediction_num]
 
     # Display the prediction
@@ -133,13 +133,13 @@ if st.button('Run MODEL'):
     
 
     # Load and display image based on prediction
-    if prediction == 'Diabetic Risk':
+    if prediction == 'High Risk in Diabetes':
         st.image('sugar-blood-level.png', width=200)
-        st.write('High Risk in Diabetes: Please consult your healthcare provider for further evaluation and management.')
+        st.write('Please consult your healthcare provider for further evaluation and management.')
         st.markdown('<p style="font-size: 12px;">All content and media on the Project Dia-BEAT-this app is created and published online for informational purposes only. It is not intended to be a substitute for professional medical advice and should not be relied on as health or personal advice.</p>', unsafe_allow_html=True)
-    elif prediction == 'Non-Diabetic':
+    elif prediction == 'Low Risk in Diabetes':
         st.image('healthy.png', width=200)
-        st.write('Low Risk in Diabetes: No immediate concern, but it is always good to maintain a healthy lifestyle and regular check-ups.')
+        st.write('No immediate concern, but it is always good to maintain a healthy lifestyle and regular check-ups.')
         st.markdown('<p style="font-size: 12px;">All content and media on the Project Dia-BEAT-this app is created and published online for informational purposes only. It is not intended to be a substitute for professional medical advice and should not be relied on as health or personal advice.</p>', unsafe_allow_html=True)
 
 
